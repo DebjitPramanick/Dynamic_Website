329badf75d0dab705f0bf771cce8c0f8685ea40c
Elaf <skelaf2000@gmail.com>
2022-08-02 23:27:16 +0530

    adding static path with public folder and also requesting express to use it via require path

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
 const express = require("express");
+const path = require("path");
 require("./db/conn");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+// middle ware setting the path
+const staticPath = path.join(__dirname, "../public");
+app.use(express.static(staticPath))
+
 //routing
 //app.get(path, callback)
+
 app.get('/',(req,res)=>{
     res.send("Hi");
 })
