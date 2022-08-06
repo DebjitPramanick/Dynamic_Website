d785ada3b9dcca5957cd22b26411a60be9218127
Elaf <skelaf2000@gmail.com>
2022-08-06 23:59:11 +0530

    hbs.registerPartials(partialpath;

--- src/app.js
+++ src/app.js
@@ -6,14 +6,19 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // setting the path
-// const staticPath = path.join(__dirname, "../public");
+const staticPath = path.join(__dirname, "../public");
+const templatepath = path.join(__dirname, "../templates/views")
+const partialpath = path.join(__dirname, "../templates/partials")
+
 
 // middleware
 app.use('/css', express.static(path.join(__dirname, "../node_modules/bootstrap/dist/css")))
 app.use('/js', express.static(path.join(__dirname, "../node_modules/bootstrap/dist/js")))
 app.use('/jq', express.static(path.join(__dirname, "../node_modules/jquery/dist")))
-// app.use(express.static(staticPath))
-app.set("view engine", "hbs");
+app.use(express.static(staticPath))
+app.set('view engine', '.hbs');
+app.set("views", templatepath);
+hbs.registerPartials(partialpath);
 
 
 //routing
