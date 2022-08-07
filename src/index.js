51d761638642387c128dbe8f47916d2c3893a26c
DebjitPramanick <dpramanick9990@gmail.com>
2022-08-07 09:57:39 +0530

    Fix: Import

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 require("./db/conn");
+const hbs = require("hbs")
 
 const app = express();
 const port = process.env.PORT || 3000;
